60eeea2b072b2817c1737d04697dafd1bebf9a37
Marcel Roesink <RSK01@rocvantwente.nl>
2023-09-10 14:31:11 +0200

    Add project files.

---
+++ WebApplication1/Controllers/Lid3Opdracht1Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public partial class Opdracht1Controller
+    {
+        public IActionResult Opdracht1c()
+        {
+            return View();
+        }
+
+        public IActionResult Opdracht1g()
+        {
+            return View();
+        }
+    }
+}
